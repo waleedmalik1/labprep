61616d06524833c2bd0e1eb665cb185babdc816c
waleedmalik1 <waleedmalik0340>
2019-07-14 20:14:55 +0500

    first commit uploading all files

---
+++ mycal/src/mycal/cal.java
@@ -0,0 +1,10 @@
+package mycal;
+
+public class cal {
+
+public int add(int a,int b)
+
+{
+	return a+b;
+}
+}
---
+++ mycal/src/mycal/test.java
@@ -0,0 +1,12 @@
+package mycal;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		cal c=new cal();
+		System.out.println(""+c.add(1, 2));
+	}
+
+}
